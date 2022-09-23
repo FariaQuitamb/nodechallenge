b2ff5dd4776abdf9865f941d1de6b363c0a65d65
FariaQuitamb <quitambadev@gmail.com>
2022-09-24 00:11:46 +0100

    fix varietes bugs

--- node/index.js
+++ node/index.js
@@ -1,44 +1,44 @@
-const { json } = require('express')
-const express = require('express')
-const app = express()
-const port = 3000
-const rows = {}
+const { json } = require("express");
+const express = require("express");
+const app = express();
+const port = 3000;
+const rows = {};
 
 const config = {
-    host: 'nodejs-database-1',
-    user: 'root',
-    password: 'root',
-    database: 'challengeDB'
-}
+  host: "nodejs-database-1",
+  user: "root",
+  password: "root",
+  database: "challengeDB",
+};
 
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
 
-const getTable = 'DROP TABLE IF EXISTS "people"';
-connection.query(getTable);
+const getTable = "DROP TABLE IF EXISTS people";
+connection.query(getTable, (err, result) => {
+  console.log("Table droped");
+  console.log(result);
+});
 
-const table = "create table people (id int not null auto_increment primary key, name varchar(40) not null)";
-connection.query(table);
+const tableCreated =
+  "create table people (id int not null auto_increment primary key, name varchar(40) not null)";
+connection.query(tableCreated);
 
+const sql = `INSERT INTO people(name) values('Faria Quitamba')`;
+connection.query(sql);
 
-                
-const sql = `INSERT INTO people(name) values('Faria Quitamba')`
-connection.query(sql)
+const getRegisters = `SELECT name FROM people`;
+connection.query(getRegisters, (err, rows, fields) => {
+  this.rows = rows;
+});
 
-const getRegisters = `SELECT name FROM people`
-connection.query(getRegisters, (err, rows, fields)=>{
-   this.rows = rows
-}) 
-
-
-
-app.get('/', (req, res)=> {
-    res.send(`
+app.get("/", (req, res) => {
+  res.send(`
         <p> Lista de nomes cadastrada no banco de dados.</p>
         ${this.rows[0].name}
-    `)
-})
+    `);
+});
 
-app.listen(port, ()=>{
-    console.log('App is running ' + port)
-})
+app.listen(port, () => {
+  console.log("App is running " + port);
+});
