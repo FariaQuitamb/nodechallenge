af0849e821a54519f098f0f582aa3d938b241dac
FariaQuitamb <quitambadev@gmail.com>
2022-09-23 23:18:28 +0100

    my nodejs, mysql and docker challenge

---
+++ node/index.js
@@ -0,0 +1,44 @@
+const { json } = require('express')
+const express = require('express')
+const app = express()
+const port = 3000
+const rows = {}
+
+const config = {
+    host: 'nodejs-database-1',
+    user: 'root',
+    password: 'root',
+    database: 'challengeDB'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const getTable = 'DROP TABLE IF EXISTS people';
+connection.query(getTable);
+
+const table = "create table people (id int not null auto_increment primary key, name varchar(40) not null)";
+connection.query(table);
+
+
+                
+const sql = `INSERT INTO people(name) values('Faria Quitamba')`
+connection.query(sql)
+
+const getRegisters = `SELECT name FROM people`
+connection.query(getRegisters, (err, rows, fields)=>{
+   this.rows = rows
+}) 
+
+
+
+app.get('/', (req, res)=> {
+    res.send(`
+        <p> Lista de nomes cadastrada no banco de dados.</p>
+        ${this.rows[0].name}
+    `)
+})
+
+app.listen(port, ()=>{
+    console.log('App is running ' + port)
+})
